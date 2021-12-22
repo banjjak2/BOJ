aa2105c12fc8edbe9ff782c405b50995b7098c5e
banjjak2 <interr0bang@naver.com>
2021-12-22 15:28:26 +0900

    [15655] N과 M (6)

---
+++ src/Silver_3/N과_M_6_15655.java
@@ -0,0 +1,48 @@
+package Silver_3;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class N과_M_6_15655 {
+    private static StringBuilder sb = new StringBuilder();
+    private static int[] result = null;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        result = new int[M];
+
+        int index = 0;
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()) {
+            arr[index++] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        solve(arr, 0, M, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void solve(int[] arr, int curIndex, int r, int c) {
+        if (c == r) {
+            for(int i : result) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        else {
+            for(int i=curIndex; i<arr.length; i++) {
+                result[c] = arr[i];
+                solve(arr, i+1, r, c+1);
+            }
+        }
+    }
+}
